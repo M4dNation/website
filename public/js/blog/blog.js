591ca290d238a247991cbd59801dc5e9b5847b55
axelvaindal <pro.axelvaindal@gmail.com>
2016-07-10 08:31:35 +0000

    Work on gitignore test

---
+++ public/js/blog/blog.js
@@ -0,0 +1,72 @@
+
+$('.gallery-item img').click(function()
+{
+	var url = $(this).data('url');
+	var alt = $(this).data('alt');
+	var id = $(this).data('id');
+
+	var image = $('#galleryModal img');
+	var title = $('#galleryModal .modal-title');
+
+	title.text(alt);
+	image.attr('src', url);
+	image.attr('alt', alt);
+	image.data('id', id);
+	$('#galleryModal').modal('toggle');
+});
+
+$('#galleryModal .next').click(function()
+{
+	var image = $('#galleryModal img');
+	var id = image.data('id');
+	var articleId = id.split("-")[0];
+	var imageId = id.split("-")[1];
+	imageId = parseInt(imageId,10);
+
+	var newImage = $('.gallery-item img[data-id="'+ articleId + '-' + (imageId+1) +'"]');
+	var url = newImage.attr('src');
+
+	if (url === undefined)
+	{
+		newImage = $('.gallery-item img[data-id="'+ articleId + '-' + (1) +'"]');
+	}
+
+	url = newImage.attr('src');
+	var alt = newImage.attr('alt');
+	id = newImage.data('id');
+	var title = $('#galleryModal .modal-title');
+
+	title.text(alt);
+	image.attr('src', url);
+	image.attr('alt', alt);
+	image.data('id', id);
+});
+
+$('#galleryModal .previous').click(function()
+{
+	var image = $('#galleryModal img');
+	var id = image.data('id');
+	var articleId = id.split("-")[0];
+	var imageId = id.split("-")[1];
+	imageId = parseInt(imageId,10);
+
+	var newImage = $('.gallery-item img[data-id="'+ articleId + '-' + (imageId-1) +'"]');
+	var url = newImage.attr('src');
+	var totalImage=$('.gallery-container[data-article="'+ articleId+'"]').data('images');
+
+	if (url === undefined)
+	{
+		newImage = $('.gallery-item img[data-id="'+ articleId + '-' + totalImage +'"]');
+	}
+
+	url = newImage.attr('src');
+	var alt = newImage.attr('alt');
+	id = newImage.data('id');
+	var title = $('#galleryModal .modal-title');
+
+	title.text(alt);
+	image.attr('src', url);
+	image.attr('alt', alt);
+	image.data('id', id);
+});
+//# sourceMappingURL=blog.js.map
