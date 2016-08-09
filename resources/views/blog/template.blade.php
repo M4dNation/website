05bfc4394668470024dc79245c1e302d782b6c04
remiHau <remi.hau@gmail.com>
2016-08-09 11:51:26 +0200

    [Fix] #1 SEO Issue Copyright Fixing the meta copyright to improve SEO

--- resources/views/blog/template.blade.php
+++ resources/views/blog/template.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
     <meta name="description" content="Official Blog of M4dNation." />
     <meta name="author" content="M4dNation" />
-	<meta name="copyright" content="All images, names or brands used are property of M4dNation SAS. All rights reserved." />
+	<meta name="copyright" content="M4dNation SAS" />
     {!! Html::style(elixir('css/lib/libraries.css')) !!}
     {!! Html::style(elixir('css/blog/blog.css')) !!}
     {!! Html::favicon('favicon.ico') !!}
--- resources/views/project/template.blade.php
+++ resources/views/project/template.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
     <meta name="description" content="Yggdrasill is a crowdsourcing platform for players and video game studios." />
     <meta name="author" content="M4dNation" />
-	<meta name="copyright" content="All images, names or brands used are property of M4dNation SAS. All rights reserved." />
+	<meta name="copyright" content="M4dNation SAS" />
     {!! Html::style(elixir('css/lib/libraries.css')) !!}
     {!! Html::style(elixir('css/project/project.css')) !!}
     {!! Html::favicon('favicon.ico') !!}
