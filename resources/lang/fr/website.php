8a443e27bea3c47c49e232e5d943cd28790cd8ab
remiHau <remi.hau@gmail.com>
2016-09-09 17:40:49 +0200

    [WIP] Fixing translation error

--- resources/lang/fr/website.php
+++ resources/lang/fr/website.php
@@ -17,9 +17,9 @@ return [
                     Aidée par sa forte connaissance du monde du jeu vidéo, <strong>M4dNation</strong>  veut faciliter la relation entre les joueurs et les studios pour créer de meilleurs jeux : les vôtres !',
     'company3' => 'En 2014, <strong>Axel Vaindal et Gabin Raffoux</strong>, co-fondateurs du studio, imaginèrent une plateforme communautaire pour les joueurs et les developeurs : <strong>Yggdrasill</strong>.',
     'company4' => 'Guidé par vos idées et la créativité des studios, le jeu participatif est maintenant <strong>une réalité !</strong>',
-    'project1' => '<strong>Yggdrasill</strong> est une plateforme de <strong>crowdsourcing</strong> pour les joueurs et les developpeurs pour leur permettre de créer plus prochez de vos envies.',
+    'project1' => '<strong>Yggdrasill</strong> est une plateforme de <strong>crowdsourcing</strong> pour les joueurs et les developpeurs pour leur permettre de créer des jeux plus proches de leurs envies.',
     'project2' => '<strong>Yggdrasill</strong> est construite autour d\'un nouveau modèle de représentation en arbre pour permettre à chacun de participer, partager et discuter de ses opinions sur chaque élément du jeu. De nombreuses fonctionnalités telles que des galeries de média et des fils de discussions sont intégrés à la plateforme pour faciliter la communication.',
-    'project3' => 'Suivez la production de vos jeux favoris <b>depuis la naissance de l\'idée à la version finale</b> grâce à un blog et une roadmap spécifique à chaque jeu et communiquez avec les développeurs grâce à l\'intégration des réseaux sociaux.',
+    'project3' => 'Suivez la production de vos jeux favoris, <b>depuis la naissance de l\'idée à la version finale</b>, grâce à un blog et une roadmap spécifique à chaque jeu et communiquez avec les développeurs grâce à l\'intégration des réseaux sociaux.',
     'project4' => 'Encore mieux, partagez vos propres jeux et regardez les grandir au sein de notre communauté afin de les faire développer par <b>les plus grands studios de jeux vidéos</b> du monde.',
     'projectLink' => 'En savoir plus',
     'team' => [
